4d75487fd4beadd1498a75fd7fcc11b6c4e86677
Sheila Samita <samitasheila1@gmail.com>
2025-05-08 18:04:07 +0300

    Implemented sequences with python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    if length == 0:
+        print(fibonacci_sequence)
+    elif length == 1:
+        fibonacci_sequence = [0]
+        print(fibonacci_sequence)
+    elif length == 2:
+        fibonacci_sequence = [0, 1]
+        print(fibonacci_sequence)
+    else:
+        fibonacci_sequence = [0, 1]
+        while len(fibonacci_sequence) < length:
+            next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_number)
+        print(fibonacci_sequence)
